68c28640ec2a1b9babd5f49bd311a9bcc4613f34
jacken <jackenliy@gmail.com>
2021-05-20 14:12:42 +0800

    扩展 Date 格式化函数

---
+++ src/javascript/date.js
@@ -0,0 +1,28 @@
+/**
+ * 对Date扩展，根据指定格式转换Date
+ * @param {*} fmt 时间格式
+ * @returns
+ */
+Date.prototype.Format = function (fmt) {
+  const o = {
+    'M+': this.getMonth() + 1, //月份
+    'D+': this.getDate(), //日
+    'H+': this.getHours(), //小时
+    'm+': this.getMinutes(), //分
+    's+': this.getSeconds(), //秒
+    'q+': Math.floor((this.getMonth() + 3) / 3), //季度
+    S: this.getMilliseconds(), //毫秒
+  };
+  if (/(Y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+  }
+  for (const k in o) {
+    if (new RegExp('(' + k + ')').test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length));
+    }
+  }
+  return fmt;
+};
+
+let date = new Date();
+console.log(date.Format('YYYY-MM-DD HH:mm:ss'));

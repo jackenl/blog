c7b80346d17172e0e6d8ee83c55e7d626c37199c
jackenl <jackenl@users.noreply.github.com>
2021-06-09 07:27:15 +0000

    Update Examples b052507a0c6ba3d80144d472902d5e28c68d2075

--- mypack/src/index.js
+++
@@ -1,3 +0,0 @@
-const print = require('./print');
-
-print();
--- mypack/src/print.js
+++
@@ -1,8 +0,0 @@
-const fun = require('./demo')
-
-function print() {
-  fun();
-  console.log('Hello world!');
-}
-
-module.exports = print;
